f33a47dedfc057dfbbfd872693d3fd26022925f1
ngo_huu_phong@management-partners.co.jp <129248598+phongngohuu@users.noreply.github.com>
2023-04-04 14:31:54 +0700

    using viper to autoload config env variable

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,22 +7,23 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:123456@localhost:7432/transfer_bank?sslmode=disable"
+	"github.com/phongngohuu/bank_transfer/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
+
 	testQueries = New(testDB)
 
 	os.Exit(m.Run())
--- main.go
+++ main.go
@@ -7,16 +7,16 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/phongngohuu/bank_transfer/api"
 	db "github.com/phongngohuu/bank_transfer/db/sqlc"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:123456@localhost:7432/transfer_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8888"
+	"github.com/phongngohuu/bank_transfer/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,8 +24,8 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
-		log.Fatal("Cannot start server:", err)
+		log.Fatal("cannot start server:", err)
 	}
 }
