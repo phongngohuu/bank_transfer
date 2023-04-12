78cf3933e13330b5ddd65f40f51f8807fc5dda19
ngo_huu_phong@management-partners.co.jp <129248598+phongngohuu@users.noreply.github.com>
2023-04-12 15:55:07 +0700

    add hash password

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -10,12 +10,17 @@ import (
 )
 
 func createRandomUser(t *testing.T) User {
+
+	hashedPassword, err := util.HashedPassword(util.RandomString(6))
+	require.NoError(t, err)
+
 	arg := CreateUserParams{
 		Username:       util.RandomOwner(),
-		HashedPassword: "secret ",
+		HashedPassword: hashedPassword,
 		FullName:       util.RandomOwner(),
 		Email:          util.RandomEmail(),
 	}
+
 	user, err := testQueries.CreateUser(context.Background(), arg)
 	require.NoError(t, err)
 	require.NotEmpty(t, user)
